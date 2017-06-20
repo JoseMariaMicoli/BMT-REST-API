2e27add2b4efad99b2b58c129198e3f018788203
JoseMariaMicoli <josemaria.micoli@gmail.com>
2017-06-20 18:12:27 -0300

    Change the servlet container from Tomcat to Wildfly 10

--- src/main/java/com/micoli/backend/BackendRestApiApplication.java
+++ src/main/java/com/micoli/backend/BackendRestApiApplication.java
@@ -3,11 +3,18 @@ package com.micoli.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class BackendRestApiApplication {
+public class BackendRestApiApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendRestApiApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendRestApiApplication.class, args);
 	}
